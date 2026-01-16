401d7e14d66f45f0224ff6be0b3122518dcc5f9c
ggVzea <xenonruxy@gmail.com>
2026-01-16 14:41:37 +0700

    Add files via upload

---
+++ Modules/Optimize.c
@@ -0,0 +1,36 @@
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Modules/refs/heads/main/V2.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local Funcs = {}
+
+LabelC = function(Section, Text)
+  return Section:AddSection(Text)
+end
+
+ToggleC = function(Section, MainTitle, Title, Description, Default)
+    local runningTask
+    _G[MainTitle] = Default or false
+
+    Section:AddToggle(MainTitle, {
+        Title = Title or "nil toggle",
+        Description = Description or "nil description",
+        Default = Default or false,
+        Callback = function(Value)
+            _G[MainTitle] = Value
+
+            if runningTask then
+                task.cancel(runningTask)
+                runningTask = nil
+            end
+
+            if Value and Funcs[MainTitle] then
+                runningTask = task.spawn(function()
+                    while _G[MainTitle] do
+                        pcall(Funcs[MainTitle])
+                        task.wait(0.15)
+                    end
+                end)
+            end
+        end
+    })
+end
